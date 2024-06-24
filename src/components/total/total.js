77b0ecc38336c7c7fe81244b06f316a7a7ee031d
Hadeer Salah <Haduirsalah@gmail.com>
2024-06-24 14:33:59 +0300

    Add a new column for 'quantity', where user can increase and decrease the quantity of product using + and - buttons

--- src/components/total/total.js
+++ src/components/total/total.js
@@ -1,16 +1,15 @@
-import React from 'react';
-
 const Total = (props) => {
-    const {items} = props;
-    let total = 0
+    const { items } = props;
+    let total = 0;
     for (let i = 0; i < items.length; i++) {
-        total += parseFloat(items[i].price)
+      total += parseFloat(items[i].price) * items[i].quantity;
     }
     return (
-        <div>
-            <p className="text">Total Price: {total}</p>
-        </div>
-    )
-}
-
-export default Total
+      <div>
+        <p className="text">Total Price: {total}</p>
+      </div>
+    );
+  }
+  
+  export default Total;
+  
